b7c67a65760019bf0223420b475933ec342e81b5
Dibujaron <ddp@integrity.com>
2014-03-17 18:53:19 -0400

    Test

--- Feudalism/src/com/dibujaron/feudalism/Feudalism.java
+++ Feudalism/src/com/dibujaron/feudalism/Feudalism.java
@@ -3,5 +3,10 @@ package com.dibujaron.feudalism;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Feudalism extends JavaPlugin{
-
+	public void onEnable(){
+		
+	}
+	public void onDisable(){
+		
+	}
 }
